36e2ad8d317898ad04ec8e0c23de7d2405dc73a6
Orlean Sousa <orleansilva60@hotmail.com>
2024-09-23 16:34:24 -0300

    decidir começar a mudar as branchs e trabalhamos com operadores logicos && e ||

--- web/js/main.js
+++ web/js/main.js
@@ -32,21 +32,32 @@
 
 //alert('A temperatura em Fahrenheit é de '+ fahrenheit.toFixed(2))
 
-let grade = prompt('Digite a nota do aluno:')
+// let grade = prompt('Digite a nota do aluno:')
 
-if (grade > 100){
-    alert('Nota invalida')
-}else if (grade >= 90){
-    alert('Aluno nota A')
-}
-else if (grade >= 80){
-    alert('Aluno nota B')
-}
-else if (grade >= 70){
-    alert('Aluno nota C')
-}
-else if (grade >= 60){
-    alert('Aluno nota D')
-}else{
-    alert('Nota F')
+// if (grade > 100){
+//     alert('Nota invalida')
+// }else if (grade >= 90){
+//     alert('Aluno nota A')
+// }
+// else if (grade >= 80){
+//     alert('Aluno nota B')
+// }
+// else if (grade >= 70){
+//     alert('Aluno nota C')
+// }
+// else if (grade >= 60){
+//     alert('Aluno nota D')
+// }else{
+//     alert('Nota F')
+// }
+
+let idade =  prompt('qual a sua idade: ')
+let titulo = prompt('tem titulo [sim/não]')
+
+if(idade >= 18 && titulo === 'sim'){
+    alert('pode votar')
+}else if(idade > 18 && titulo === 'não' || titulo === 'nao'){
+    alert('precisa de titulo')
+}else {
+    alert('não pode votar!')
 }
