6f7ea8dea035b8147b95d1b6107f08aeca185eec
Orlean Sousa <orleansilva60@hotmail.com>
2024-09-23 17:26:12 -0300

    enviando resolução do desafio dado pelo professor

--- web/js/main.js
+++ web/js/main.js
@@ -1,9 +1,9 @@
 let colorList = []
 
 for ( let i = 1; i <= 3; i++){
-    let userColor = prompt('digite a cor: ')
+    let userColor = prompt('digite a cor: ' + i)
     colorList.push(userColor)
     
 }
 
-alert('A sua lista de cores ficou assim: ' + colorList)
+document.getElementById('coresLista').innerHTML = colorList
