8981c5e018067d9c5958746ba039494248bce279
Orlean Sousa <orleansilva60@hotmail.com>
2024-09-23 15:11:15 -0300

    adicionando logica e manipulação de tipos de variaveis. alem de trabalhar com prompot

--- web/js/main.js
+++ web/js/main.js
@@ -1,7 +1,7 @@
-let fristName = "Orlean"
-let lastName = "Sousa"
-
-let age = 28
-
-console.log('Meu nome é ' + fristName + ' ' + lastName)
-console.log('Eu tenho ' + age + ' anos de idade')
+let firstName = prompt('Digite o seu primeiro nome: ')
+let lastName = prompt('Digite o seu sobrenome: ')
+let age = parseInt( prompt('Digite a sua idade: '))
+ageFinal = age + 2 
+console.log(firstName)
+console.log(lastName)
+console.log(ageFinal )
