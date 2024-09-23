a73a58e1abbb321dd12289ae7877b83f4549773a
Orlean Sousa <orleansilva60@hotmail.com>
2024-09-23 16:51:34 -0300

    demostração de listas em html

--- web/js/main.js
+++
@@ -1,8 +0,0 @@
-let valorConta = parseFloat(prompt('Qual o valor da conta: '))
-let gorjeta = parseFloat(prompt('Digite a porcentagem da gorjeta: '))
-
-
-gorjeta = (valorConta * gorjeta) / 100
-let valorFinal = valorConta + gorjeta
-
-alert('O valor total da conta com gorjeta é: ' + valorFinal.toFixed(2))
