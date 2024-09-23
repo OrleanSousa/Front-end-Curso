a38c6c8e3e945c7b2025c7234c542d498bd94c97
Orlean Sousa <orleansilva60@hotmail.com>
2024-09-23 15:25:13 -0300

    explicação de estilização utilizando o arquivo css

--- web/js/main.js
+++ web/js/main.js
@@ -1,11 +1,11 @@
-let firstName = prompt('Digite o seu primeiro nome: ')
-let lastName = prompt('Digite o seu sobrenome: ')
-let age = parseInt( prompt('Digite a sua idade: '))
+//let firstName = prompt('Digite o seu primeiro nome: ')
+//let lastName = prompt('Digite o seu sobrenome: ')
+//let age = parseInt( prompt('Digite a sua idade: '))
 
-const ageFixed = 2
+//const ageFixed = 2
 
-ageFinal = age + ageFixed
+//ageFinal = age + ageFixed
 
-console.log(firstName)
-console.log(lastName)
-console.log(ageFinal )
+//console.log(firstName)
+//console.log(lastName)
+//console.log(ageFinal )
