fb1c333a942449270986818e07c77d1085d24ab4
Orlean Sousa <orleansilva60@hotmail.com>
2024-09-23 17:25:02 -0300

    apresentação do loop for e desafio

---
+++ web/js/main.js
@@ -0,0 +1,9 @@
+let colorList = []
+
+for ( let i = 1; i <= 3; i++){
+    let userColor = prompt('digite a cor: ')
+    colorList.push(userColor)
+    
+}
+
+alert('A sua lista de cores ficou assim: ' + colorList)
