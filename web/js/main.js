d20da5ad4fd93640632f97ff370fab40289a7181
Orlean Sousa <orleansilva60@hotmail.com>
2024-09-25 09:28:25 -0300

    relogio de hora atual

--- web/js/main.js
+++ web/js/main.js
@@ -1,31 +1,12 @@
-function calculate(){
-  var bmi;
-  var result = document.getElementById("result");
+const clockElement = document.getElementById('clock');
 
-  var weight = parseInt(document.getElementById("weight").value);
-  document.getElementById("weight-val").textContent = weight + " kg";
-
-  var height = parseInt(document.getElementById("height").value);
-  document.getElementById("height-val").textContent = height + " cm";
-
-  bmi = (weight / Math.pow( (height/100), 2 )).toFixed(1);
-  result.textContent = bmi;
-  
-  if(bmi < 18.5){
-      category = "Underweight";
-      result.style.color = "#ffc44d";
-  }
-  else if( bmi >= 18.5 && bmi <= 24.9 ){
-      category = "Normal Weight";
-      result.style.color = "#0be881";
-  }
-  else if( bmi >= 25 && bmi <= 29.9 ){
-      category = "Overweight";
-      result.style.color = "#ff884d";
-  }
-  else{
-      category = "Obese";
-      result.style.color = "#ff5e57";
-  }
-  document.getElementById("category").textContent = category;
+function updateClock() {
+  const date = new Date();
+  const hours = date.getHours().toString().padStart(2, '0');
+  const minutes = date.getMinutes().toString().padStart(2, '0');
+  const seconds = date.getSeconds().toString().padStart(2, '0');
+  clockElement.textContent = `${hours}:${minutes}:${seconds}`;
 }
+
+updateClock();
+setInterval(updateClock, 1000);
