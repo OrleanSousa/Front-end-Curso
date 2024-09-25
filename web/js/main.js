f8cd739ad6177159f9d18bd6bdb6e90ee5f19ce0
Orlean Sousa <orleansilva60@hotmail.com>
2024-09-25 09:07:55 -0300

    aprendendo a criar pop-up

--- web/js/main.js
+++ web/js/main.js
@@ -1,111 +1,17 @@
-const calculator = {
-  displayValue: '0',
-  firstOperand: null,
-  waitingForSecondOperand: false,
-  operator: null,
-};
+const openModalButton = document.getElementById("openModal");
+const modal = document.getElementById("modal");
+const closeModalButton = document.getElementsByClassName("close")[0];
 
-function inputDigit(digit) {
-  const { displayValue, waitingForSecondOperand } = calculator;
+openModalButton.addEventListener("click", function() {
+  modal.style.display = "block"; // Show the modal
+});
 
-  if (waitingForSecondOperand === true) {
-    calculator.displayValue = digit;
-    calculator.waitingForSecondOperand = false;
-  } else {
-    calculator.displayValue = displayValue === '0' ? digit : displayValue + digit;
+closeModalButton.addEventListener("click", function() {
+  modal.style.display = "none"; // Hide the modal
+});
+
+window.addEventListener("click", function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none"; // Hide the modal when the user clicks outside of it
   }
-}
-
-function inputDecimal(dot) {
-  if (calculator.waitingForSecondOperand === true) {
-      calculator.displayValue = "0."
-    calculator.waitingForSecondOperand = false;
-    return
-  }
-
-  if (!calculator.displayValue.includes(dot)) {
-    calculator.displayValue += dot;
-  }
-}
-
-function handleOperator(nextOperator) {
-  const { firstOperand, displayValue, operator } = calculator
-  const inputValue = parseFloat(displayValue);
-  
-  if (operator && calculator.waitingForSecondOperand)  {
-    calculator.operator = nextOperator;
-    return;
-  }
-
-
-  if (firstOperand == null && !isNaN(inputValue)) {
-    calculator.firstOperand = inputValue;
-  } else if (operator) {
-    const result = calculate(firstOperand, inputValue, operator);
-
-    calculator.displayValue = `${parseFloat(result.toFixed(7))}`;
-    calculator.firstOperand = result;
-  }
-
-  calculator.waitingForSecondOperand = true;
-  calculator.operator = nextOperator;
-}
-
-function calculate(firstOperand, secondOperand, operator) {
-  if (operator === '+') {
-    return firstOperand + secondOperand;
-  } else if (operator === '-') {
-    return firstOperand - secondOperand;
-  } else if (operator === '*') {
-    return firstOperand * secondOperand;
-  } else if (operator === '/') {
-    return firstOperand / secondOperand;
-  }
-
-  return secondOperand;
-}
-
-function resetCalculator() {
-  calculator.displayValue = '0';
-  calculator.firstOperand = null;
-  calculator.waitingForSecondOperand = false;
-  calculator.operator = null;
-}
-
-function updateDisplay() {
-  const display = document.querySelector('.calculator-screen');
-  display.value = calculator.displayValue;
-}
-
-updateDisplay();
-
-const keys = document.querySelector('.calculator-keys');
-keys.addEventListener('click', event => {
-  const { target } = event;
-  const { value } = target;
-  if (!target.matches('button')) {
-    return;
-  }
-
-  switch (value) {
-    case '+':
-    case '-':
-    case '*':
-    case '/':
-    case '=':
-      handleOperator(value);
-      break;
-    case '.':
-      inputDecimal(value);
-      break;
-    case 'all-clear':
-      resetCalculator();
-      break;
-    default:
-      if (Number.isInteger(parseFloat(value))) {
-        inputDigit(value);
-      }
-  }
-
-  updateDisplay();
 });
